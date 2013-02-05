9bbcfa7bbe308eb02a608e00f78c6a49193e2063
Michael Waskom <mwaskom@stanford.edu>
2013-02-05 12:07:49 -0800

    Starting to build new experiment, still mostly a mess

--- experiment.js
+++ experiment.js
@@ -1,18 +1,6 @@
-// I'm implementing the experiment using a data structure that I call a **sequence**. The insight behind sequences is that many experiments consist of a sequence of largely homogeneous trials that vary based on a parameter. For instance, in this example experiment, a lot stays the same from trial to trial - we always have to present some number, the subject always has to make a response, and we always want to record that response. Of course, the trials do differ - we're displaying a different number every time. The idea behind the sequence is to separate what stays the same from what differs - to **separate code from data**. This results in **parametric code**, which is much easier to maintain - it's simple to add, remove, or change conditions, do randomization, and do testing.
-
-// ## High-level overview
-// Things happen in this order:
-// 
-// 1. Compute randomization parameters (which keys to press for even/odd and trial order), fill in the template <code>{{}}</code> slots that indicate which keys to press for even/odd, and show the instructions slide.
-// 2. Set up the experiment sequence object.
-// 3. When the subject clicks the start button, it calls <code>experiment.next()</code>
-// 4. <code>experiment.next()</code> checks if there are any trials left to do. If there aren't, it calls <code>experiment.end()</code>, which shows the finish slide, waits for 1.5 seconds, and then uses mmturkey to submit to Turk.
-// 5. If there are more trials left, <code>experiment.next()</code> shows the next trial, records the current time for computing reaction time, and sets up a listener for a key press.
-// 6. The key press listener, when it detects either a P or a Q, constructs a data object, which includes the presented stimulus number, RT (current time - start time), and whether or not the subject was correct. This entire object gets pushed into the <code>experiment.data</code> array. Then we show a blank screen and wait 500 milliseconds before calling <code>experiment.next()</code> again.
-
 // ## Helper functions
 
-// Shows slides. We're using jQuery here - the **$** is the jQuery selector function, which takes as input either a DOM element or a CSS selector string.
+// Shows slides. 
 function showSlide(id) {
   // Hide all slides
 	$(".slide").hide();
@@ -21,19 +9,49 @@ function showSlide(id) {
 }
 
 // Get random integers.
-// When called with no arguments, it returns either 0 or 1. When called with one argument, *a*, it returns a number in {*0, 1, ..., a-1*}. When called with two arguments, *a* and *b*, returns a random value in {*a*, *a + 1*, ... , *b*}.
-function random(a,b) {
+// When called with no arguments, it returns either 0 or 1.
+// When called with one argument, *a*, it returns a number in {*0, 1, ..., a-1*}.
+// When called with two arguments, *a* and *b*, returns a random value in 
+// {*a*, *a + 1*, ... , *b*}.
+function randint(a, b) {
 	if (typeof b == "undefined") {
 		a = a || 2;
-		return Math.floor(Math.random()*a);
+		return Math.floor(Math.random() * a);
 	} else {
-		return Math.floor(Math.random()*(b-a+1)) + a;
+		return Math.floor(Math.random() * (b-a+1)) + a;
 	}
 }
 
-// Add a random selection function to all arrays (e.g., <code>[4,8,7].random()</code> could return 4, 8, or 7). This is useful for condition randomization.
+// Draw from a gaussian distribution with optional mean and var
+// (mean, var default to 0, 1)
+function randn(mu, sigma2) {
+	var x1, x2, rad;
+ 
+	do {
+		x1 = 2 * Math.random() - 1;
+		x2 = 2 * Math.random() - 1;
+		rad = x1 * x1 + x2 * x2;
+	} while(rad >= 1 || rad == 0);
+ 
+	if (typeof mu == "undefined") {
+		mu = 0;
+        }
+	if (typeof sigma2 == "undefined") {
+		sigma2 = 1;
+        }
+	var c = Math.sqrt(-2 * Math.log(rad) / rad);
+     	X = x1 * c;
+        X = mu + Math.sqrt(sigma2) * X;
+
+    return X;
+};
+
+
+// Add a random selection function to all arrays 
+// (e.g., <code>[4,8,7].random()</code> could return 4, 8, or 7).
+// This is useful for condition randomization.
 Array.prototype.random = function() {
-  return this[random(this.length)];
+  return this[randint(this.length)];
 }
 
 // ## Configuration settings
@@ -47,40 +65,84 @@ var allKeyBindings = [
     myTrialOrder = allTrialOrders.random(),
     pOdd = (myKeyBindings["p"] == "odd");
     
-// Fill in the instructions template using jQuery's <code>html()</code> method. In particular,
-// let the subject know which keys correspond to even/odd. Here, I'm using the so-called **ternary operator**, which is a shorthand for <code>if (...) { ... } else { ... }</code>
-
-$("#odd-key").html(pOdd ? "P" : "Q");
-$("#even-key").html(pOdd ? "Q" : "P");
-
 // Show the instructions slide -- this is what we want subjects to see first.
 showSlide("instructions");
 
+// Set up the things we'll need for the main experiment
+stim = $("canvas")[0].getContext("2d")
 
-// ## The main event
-// I implement the sequence as an object with properties and methods. The benefit of encapsulating everything in an object is that it's conceptually coherent (i.e. the <code>data</code> variable belongs to this particular sequence and not any other) and allows you to **compose** sequences to build more complicated experiments. For instance, if you wanted an experiment with, say, a survey, a reaction time test, and a memory test presented in a number of different orders, you could easily do so by creating three separate sequences and dynamically setting the <code>end()</code> function for each sequence so that it points to the next. **More practically, you should stick everything in an object and submit that whole object so that you don't lose data (e.g. randomization parameters, what condition the subject is in, etc). Don't worry about the fact that some of the object properties are functions -- mmturkey (the Turk submission library) will strip these out.**
+fillBG = function(){
+    stim.fillStyle = "gray";
+    stim.fillRect(0, 0, stim.canvas.width, stim.canvas.height);
+}
+
+drawCircle = function(x, y, d, color){
+    stim.beginPath();
+    stim.arc(x, y, d / 2, 0, 2 * Math.PI);
+    stim.closePath();
+    stim.fillStyle = color;
+    stim.fill();
+}
+
+var origSize = 50;
+sizeSlider = function(evt){
+    curPos = evt.pageY
+    newSize = origSize - (curPos - mouseStart);
+    fillBG()
+    drawCircle(200, 200, newSize, "dodgerblue")
+}
+
+var mouseStart = null;
+startSliding = function(evt){
+    window.mouseStart = evt.pageY;
+    document.onmousemove = sizeSlider;
+}
+
+// Here's the top-level function for all this business that
+// actually gets executed by the experiment code
+testMemory = function(){
+
+}
+
 
 var experiment = {
+
+  // Set up an empty array to store the experimental data as we go
+  data: [],
+
+  // The end function completes the experiment and submits to Turk
+  end: function() {
+    showSlide("finished");
+    setTimeout(function() { turk.submit(old_experiment) }, 1500);
+  },
+
+  // The next function is called on each trial
+  // Basic stages are sample, delay, test
+  // Each of these stages has its own funtion to do the bulk of the work
+  next: function() {
+
+    fillBG()
+    // draw some circles
+
+    // Do the 
+    fillBG()
+    setTimeout(testMemory, 1500)
+
+  }
+
+
+var old_experiment = {
   // Parameters for this sequence.
   trials: myTrialOrder,
   // Experiment-specific parameters - which keys map to odd/even
   keyBindings: myKeyBindings,
-  // An array to store the data that we're collecting.
-  data: [],
-  // The function that gets called when the sequence is finished.
-  end: function() {
-    // Show the finish slide.
-    showSlide("finished");
-    // Wait 1.5 seconds and then submit the whole experiment object to Mechanical Turk (mmturkey filters out the functions so we know we're just submitting properties [i.e. data])
-    setTimeout(function() { turk.submit(experiment) }, 1500);
-  },
   // The work horse of the sequence - what to do on every trial.
   next: function() {
-    // Get the current trial - <code>shift()</code> removes the first element of the array and returns it.
-    var n = experiment.trials.shift();
-    // If the current trial is undefined, it means the trials array was empty, which means that we're done, so call the end function.
+    // Get the current trial
+    var n = old_experiment.trials.shift();
+    // If the current trial is undefined, we're done
     if (typeof n == "undefined") {
-      return experiment.end();
+      return old_experiment.end();
     }
     
     // Compute the correct answer.
@@ -88,45 +150,23 @@ var experiment = {
     
     showSlide("stage");
     // Display the number stimulus.
-    $("#number").html(n);
-    
-    // Get the current time so we can compute reaction time later.
-    var startTime = (new Date()).getTime();
+    var colors = Array("firebrick", "darkgreen", "midnightblue");
+    color = colors.random()
+    // Just try drawing some circles for now
 
-    
-    // Set up a function to react to keyboard input. Functions that are used to react to user input are called *event handlers*. In addition to writing these event handlers, you have to *bind* them to particular events (i.e., tell the browser that you actually want the handler to run when the user performs an action). Note that the handler always takes an <code>event</code> argument, which is an object that provides data about the user input (e.g., where they clicked, which button they pressed).
-    var keyPressHandler = function(event) {
-      // A slight disadvantage of this code is that you have to test for numeric key values; instead of writing code that expresses "*do X if 'Q' was pressed*", you have to do the more complicated "*do X if the key with code 80 was pressed*". A library like [Keymaster][keymaster], or [zen][zen] (my library, and a work in progress) lets you write simpler code like <code>key('a', function(){ alert('you pressed a!') })</code>, but I've omitted it here. Here, we get the numeric key code from the event object
-      // [keymaster]: http://github.com/madrobby/keymaster
-      // [zen]: http://github.com/longouyang/zenjs
-      var keyCode = event.which;
-      
-      if (keyCode != 81 && keyCode != 80) {
-        // If a key that we don't care about is pressed, re-attach the handler (see the end of this script for more info)
-        $(document).one("keydown", keyPressHandler);
-        
-      } else {
-        // If a valid key is pressed (code 80 is p, 81 is q),
-        // record the reaction time (current time minus start time), which key was pressed, and what that means (even or odd).
-        var endTime = (new Date()).getTime(),
-            key = (keyCode == 80) ? "p" : "q",
-            userParity = experiment.keyBindings[key],
-            data = {
-              stimulus: n,
-              accuracy: realParity == userParity ? 1 : 0,
-              rt: endTime - startTime
-            };
-        
-        experiment.data.push(data);
-        // Temporarily clear the number.
-        $("#number").html("");
-        // Wait 500 milliseconds before starting the next trial.
-        setTimeout(experiment.next, 500);
-      }
-    };
-    
-    // Here, we actually bind the handler. We're using jQuery's <code>one()</code> function, which ensures that the handler can only run once. This is very important, because generally you only want the handler to run only once per trial. If you don't bind with <code>one()</code>, the handler might run multiple times per trial, which can be disastrous. For instance, if the user accidentally presses P twice, you'll be recording an extra copy of the data for this trial and (even worse) you will be calling <code>experiment.next</code> twice, which will cause trials to be skipped! That said, there are certainly cases where you do want to run an event handler multiple times per trial. In this case, you want to use the <code>bind()</code> and <code>unbind()</code> functions, but you have to be extra careful about properly unbinding.
-    $(document).one("keydown", keyPressHandler);
-    
+    var viewer = $("canvas")[1];
+    var circle = drawCircle(viewer, 300, 200, 50, color);
+    changeSize = function(evt){
+        //circle.scale(2, 2);
+        r = viewer.height - evt.pageY;
+        console.log(r)
+        drawCircle(viewer, 300, 200, r, color);
+    }
+    //document.onmousemove = changeSize;
+    //setTimeout("var evt = {pageX: .5}; changeSize(evt);", 50)
+
+    //setTimeout(experiment.next, 1000);
+    //$(document).mousemove(changeSize);
+    //$(document).onclick(experiment.next());
   }
 }
