cc0f381b919a867c17537a1a80bd4fea17e37da5
Michael Waskom <mwaskom@stanford.edu>
2013-02-06 15:25:24 -0800

    Cleaning up slider logic and collecting data

--- experiment.js
+++ experiment.js
@@ -57,35 +57,40 @@ testMemory = function(trialData){
   window.testData.xloc = trialData.targ_xloc;
   window.testData.yloc = trialData.targ_yloc;
   window.testData.origSize = randint(10, 85);
-  drawCircle(testData.targ_xloc,
-             testData.targ_yloc,
+  drawCircle(testData.xloc,
+             testData.yloc,
              testData.origSize,
              "black");
-  document.onmousemove = startSliding;
-}
-
-startSliding = function(evt){
-  //Begin the size report
-  window.mouseStart = evt.pageY;
   document.onmousemove = sizeSlider;
+  document.onmousedown = saveSize;
 }
 
 sizeSlider = function(evt){
-  //Let the participant report the size of the target stim
+  //Let the participant use mouse to report the size of the target stim
+  if (window.mouseStart == null) {
+    console.log(evt.pageY);
+    window.mouseStart = evt.pageY;
+    curIndex = experiment.data.length - 1;
+    experiment.data[curIndex].reportStart = (new Date()).getTime();
+  }
+
   curPos = evt.pageY;
-  newSize = testData.origSize - (curPos - mouseStart);
-  window.testData.curSize = newSize
-  fillBG()
+  newSize = testData.origSize - (curPos - window.mouseStart);
+  window.testData.curSize = newSize;
+  fillBG();
   drawCircle(testData.xloc,
              testData.yloc,
              testData.curSize,
-             "black")
-  document.onmousedown = saveSize;
+             "black");
 }
 
 saveSize = function(evt){
   //Lock in the response about the test stimulus size  
   document.onmousemove = null;
+  window.mouseStart = null;
+  curIndex = experiment.data.length - 1;
+  experiment.data[curIndex].reportSize = testData.curSize;
+  experiment.data[curIndex].reportFinish = (new Date()).getTime();
   experiment.next();
 }
 
@@ -101,16 +106,16 @@ var stim = $("canvas")[0].getContext("2d")
 var trials = [];
 var used_trials = [];
 while (trials.length < design.length) {
-    to_use = randint(design.length);
-    if (used_trials.indexOf(to_use) == -1) {
-        this_trial = design[to_use];
-        this_id = this_trial.id;
-        last_index = trials.length -1
-        last_id = last_index > -1 ? trials[last_index].id : "causefalse";
-        if (this_id != last_id) {
-            trials.push(this_trial);
-        }
+  to_use = randint(design.length);
+  if (used_trials.indexOf(to_use) == -1) {
+    this_trial = design[to_use];
+    this_id = this_trial.id;
+    last_index = trials.length -1
+    last_id = last_index > -1 ? trials[last_index].id : "causefalse";
+    if (this_id != last_id) {
+      trials.push(this_trial);
     }
+  }
 }
 
 // Show the instructions slide
@@ -128,7 +133,7 @@ var experiment = {
   end: function() {
     // Complete the experiment and submit to Turk
     showSlide("finished");
-    setTimeout(function() { turk.submit(old_experiment) }, 1500);
+    setTimeout(function() { turk.submit(experiment) }, 1500);
   },
 
 
@@ -141,6 +146,8 @@ var experiment = {
     var trial = this.trials.shift();
     //`trial` will be undefined if the experiment is over
     if (typeof trial == "undefined") { return this.end() }
+
+    this.data.push(trial);
 
     //Go through the stages of the trial
     fillBG();
